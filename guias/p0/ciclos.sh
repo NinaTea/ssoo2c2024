b679113b8226e3fbce54d2742494bff1d0c376db
Camila Gallo <camilagallo1700@gmail.com>
2024-08-16 16:28:01 -0300

    bash terminado

--- guias/p0/ciclos.sh
+++ guias/p0/ciclos.sh
@@ -1,9 +1,9 @@
 # arrays (elem elem elem)
-
-mi_arr = (1 2 "tres" "cuatro" 5) # instancio array
+# si no pegas el = te tira syntax error
+mi_arr=(1 2 "tres" "cuatro" 5) # instancio array
 declare -a array_vacio # declaro array vacio
 
-mi_arr += ("six") # agrego elemento
+mi_arr+=("six") # agrego elemento
 echo ${mi_array[0]} # imprimo el primer elemento
 echo ${mi_array[@]} # imprimo todos los elementos
 
@@ -25,3 +25,13 @@ N=6
 for((i=0; i<=$N; i++));do
     echo $if
 done
+
+# ej 9 c 
+# yo en home no tengo nada, no se de que archivo tabla.csv se refieren
+# no es lo que pedian pero ñe
+while IFS="," read -r org type location name division source
+do
+    echo "El nombres de la organizacion es $name y queda en $location " >> parseo.txt
+done < Organization_v3.csv
+
+# > sobreescribe, >> appendea a lo que ya existe
